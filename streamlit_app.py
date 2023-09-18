f1981529bf6e099267dc8f26f8058f8002c409ad
swapna174 <145145030+swapna174@users.noreply.github.com>
2023-09-17 22:37:20 -0400

    Update streamlit_app.py
    Pre-Populating the Pick List

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,5 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacodo','Cherries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacodo','Strawberries'])
 streamlit.dataframe(my_fruit_list)
