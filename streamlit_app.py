9d01d0062b3a5bd7a47c5e61e57188dacf4f6893
swapna174 <145145030+swapna174@users.noreply.github.com>
2023-09-17 22:07:19 -0400

    Update streamlit_app.py
    added Desc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega and Blueberry oats')
+streamlit.text('Kale,Spinach and SMothhie')
+streamlit.text('Hard Boilded Free Caged  Eggs')
