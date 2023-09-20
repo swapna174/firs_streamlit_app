0547b5bd72b3eb7472dcd4a4fc7e9c4dda5e903a
swapna174 <145145030+swapna174@users.noreply.github.com>
2023-09-20 11:46:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,17 +36,21 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-    streamlit.stop()
 
-
-  streamlit.stop()
+streamlit.stop()
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+  my_cur.execute("select *from fruit_load_list")
+  return my_cur.fetchall()
+  
+if streamlit.button('Get Fruit Load List'): 
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = getfruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
 
 
@@ -57,7 +61,6 @@ streamlit.write('Thanks for adding jackfruit enjoy! ', fruit_choice)
 
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
 
 
 
