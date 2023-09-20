8bb60e6c4af36984b78b9e6f0cc8dcd22fcdc42b
swapna174 <145145030+swapna174@users.noreply.github.com>
2023-09-20 10:47:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,5 +38,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
-streamlit.datframe(my_data_row)
+streamlit.dataframe(my_data_row)
 
